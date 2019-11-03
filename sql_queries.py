8b8439fbbd7add54c16fe5ce9bc166428bbea048
swaption2009 <swaption2009@gmail.com>
2019-11-03 13:08:40 -0800

    Update sql_queries.py
    Change ON CONFLICT on `songplays` and `users` tables.

--- sql_queries.py
+++ sql_queries.py
@@ -59,19 +59,19 @@ CREATE TABLE IF NOT EXISTS time (timestamp BIGINT NOT NULL PRIMARY KEY,
 songplay_table_insert = ("""
 INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
 VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
-ON CONFLICT (user_id) DO NOTHING;
+ON CONFLICT (user_id) DO UPDATE SET level=EXCLUDED.level;
 """)
 
 user_table_insert = ("""
 INSERT INTO users (user_id, first_name, last_name, gender, level)
 VALUES (%s, %s, %s, %s, %s);
-ON CONFLICT (user_id) DO UPDATE SET user_id=EXCLUDED.user_id;
+ON CONFLICT (user_id) DO NOTHING;
 """)
 
 song_table_insert = ("""
 INSERT INTO songs (song_id, title, artist_id, year, duration)
 VALUES(%s, %s, %s, %s, %s);
-ON CONFLICT (song_id) DO UPDATE SET song_id=EXCLUDED.song_id;
+ON CONFLICT (song_id) DO NOTHING;
 """)
 
 artist_table_insert = ("""
